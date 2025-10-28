f3cad1871d1a6874bbcba2f594b6a5c8d2b1a542
sigmaJakob <jakob.escueta123@gmail.com>
2025-10-28 14:05:01 -0400

    Update EnemyMoveScript.cs

--- EnemyMoveScript.cs
+++ EnemyMoveScript.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public GameObject player;
+
+    public float moveSpeed = 150f;
+    public float detectionRadius = 10f;
+
+    public float damageAmount = 10f;
+
+
+
+
+    private void Start()
     {
-        
+        player = GameObject.Find("crocodile");
     }
 
+
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= detectionRadius)
+        {
+            MoveTowardsPlayer();
+        }
+    }
+    
+
+
+    private void MoveTowardsPlayer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        transform.position += direction * moveSpeed * Time.deltaTime;
     }
 }
